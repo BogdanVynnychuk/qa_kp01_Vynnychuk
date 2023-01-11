2dcb0614eef378f48ad484e3cfb1713718b660f6
BogdanVynnychuk <Bogdan.vynnichuk@gmail.com>
2023-01-11 19:59:34 +0200

    api implementation done

---
+++ lab3/main.py
@@ -0,0 +1,21 @@
+import sys
+import pip._vendor.requests as requests
+
+n = len(sys.argv)
+
+Request = "http://localhost:8888/" + sys.argv[2] + "?"
+
+
+response = ""
+if sys.argv[1] == "get":
+	response = requests.get(Request)
+if sys.argv[1] == "post":
+	response = requests.post(Request)
+if sys.argv[1] == "patch":
+	response = requests.patch(Request)
+if sys.argv[1] == "delete":
+	response = requests.delete(Request)
+
+
+print("Status code:", response.status_code)
+print("Response:", response.json())
